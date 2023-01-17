d8c5b1411faa6d71c523d853d0010e4ce6cffc97
Gion28 <sugionokurniawan01@gmail.com>
2023-01-17 22:21:04 +0700

    [Gion] Refactor request body for logger

--- src/middlewares/LoggerHandler.js
+++ src/middlewares/LoggerHandler.js
@@ -5,21 +5,21 @@ import logger from '../../config/logger/ampLog';
  * Configure logger to write log
  */
 const loggerHandler = (app) => {
-    const originalSend = app.response.send;
-    // eslint-disable-next-line no-param-reassign
-    app.response.send = function sendOverWrite(body) {
-        originalSend.call(this, body);
-        this.morgan_body_response = body;
-    };
+  const originalSend = app.response.send;
+  // eslint-disable-next-line no-param-reassign
+  app.response.send = function sendOverWrite(body) {
+    originalSend.call(this, body);
+    this.morgan_body_response = body;
+  };
 
-    morgan.token('req-body', (req) => {
-        return JSON.stringify(req.body) && JSON.stringify(req.body).replace(/["]/g, '\'');
-    });
-    morgan.token('res-body', (req, res) => {
-        return res.morgan_body_response.replace(/["]/g, '\'');
-    });
+  morgan.token('req-body', (req) => {
+    return JSON.stringify(req.body) && JSON.stringify(req.body).replace(/["]/g, '\'');
+  });
+  morgan.token('res-body', (req, res) => {
+    return res.morgan_body_response;
+  });
 
-    return morgan(':method :url :date[web] :status :user-agent :response-time ms request-body: :req-body - response-body: :res-body', { immediate: false, stream: logger.stream });
+  return morgan(':method :url :date[web] :status :user-agent :response-time ms request-body: :req-body - response-body: :res-body', { immediate: false, stream: logger.stream });
 };
 
 export default loggerHandler;
