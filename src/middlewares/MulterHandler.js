01416a20a475c5066a9e917ce513942545fa7af9
Gion28 <sugionokurniawan01@gmail.com>
2023-01-28 08:38:21 +0700

    [Gion] Refactor functionality multer to add validation

--- src/middlewares/MulterHandler.js
+++ src/middlewares/MulterHandler.js
@@ -1,12 +1,28 @@
 import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${file.fieldname}-${Date.now()}`);
-    }
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  }
 });
 
-export const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg'
+      || file.mimetype === 'image/png'
+      || file.mimetype === 'image/jpg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+export const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter
+});
